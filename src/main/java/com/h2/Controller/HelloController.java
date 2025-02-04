0df24ca73f531fdc6834ba1f28eec7378517f19a
Asad Saud <asadsaud2702@gmail.com>
2025-02-04 19:51:25 +0530

    Instentiate Spring Scurity

---
+++ src/main/java/com/h2/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.h2.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HelloController {
+
+    @GetMapping("/")
+    public String Greeting() {
+        return "Hello";
+    }
+}
